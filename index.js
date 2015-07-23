a5314301fe3d8ac7a225ffb2a76924db3cce64c4
Michael Williams <dinosaur@riseup.net>
2015-07-23 20:59:59 +1200

    lexidsya

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = frequencies
 function frequencies (input) {
   var data = new Float32Array(input.data.length)
   for (var i = 0; i < input.length; i++) {
-      data[i] = Math.min(1, Math.max(-1, data.input[i]))
+      data[i] = Math.min(1, Math.max(-1, input.data[i]))
   }
   
   var reals = ndarray(data, input.shape, input.stride, input.offset)
