55a3b46590565dfbad7b6b0862db5d714fa6863d
Michael Williams <dinosaur@riseup.net>
2015-07-24 13:41:43 +1200

    allow passing in through opts

--- stream.js
+++ stream.js
@@ -4,8 +4,8 @@ var frequencies = require('./')
 
 module.exports = frequenciesStream
 
-function frequenciesStream () {
-  return through.obj(function (input, enc, cb) {
+function frequenciesStream (opts) {
+  return through.obj(opts, function (input, enc, cb) {
     cb(null, frequencies(input))
   })
 }
